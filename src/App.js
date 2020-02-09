d13b16b64e54d352a8a268d61350395e0e8cedb2
Yoshi1980 <christopher.marston@hotmail.co.uk>
2020-02-09 20:31:42 +0000

    Added router recipe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
 
 function App() {
-  return <div className='App'>Hello from the app component</div>;
+  return (
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+    </Switch>
+  );
 }
 
 export default App;
